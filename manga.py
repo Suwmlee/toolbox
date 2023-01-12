d31765e5838980ad2d01500f48d495ee2ff357a8
Mathhew <suwmlee@gmail.com>
2023-01-12 15:38:07 +0800

    update

--- manga.py
+++ manga.py
@@ -1,3 +1,5 @@
+#!/bin/python3
+# -*- coding: utf-8 -*-
 
 import os
 import re
@@ -6,51 +8,153 @@ from utils import loadConfig, regexMatch, renamefile, replaceParentheses, zipfol
 DEBUG_MODE = True
 
 
-def moveTachiyomiManga(src, dst):
-    """ 整理tachiyomi
-    """
-    if not os.path.exists(src) or not os.path.exists(dst):
-        print(f"目录异常 {src} {dst}")
-        return
-    dirs = os.listdir(src)
-    for entry in dirs:
-        full = os.path.join(src, entry)
-        if os.path.isdir(full):
-            if entry == '@eaDir':
-                print("忽略群晖文件夹")
-                continue
-            modified = checkTachiyomiManga(full, dst)
-            if not modified:
-                print("跳过... \n")
-                continue
-            print("开始压缩...")
-            for key in modified.keys():
-                cbzfile = key + '.cbz'
-                dest = modified.get(key) + '.zip'
-                if DEBUG_MODE:
-                    print(f"测试模式： {cbzfile} 到 {dest}")
+class MangaInfo():
+
+    def __init__(self, root: str):
+        self.isManga = True
+        self.fullPath = root
+        self.name = os.path.basename(root)
+        self.chapters = findMangaChapter(root)
+        # 检测单本，多本
+        if len(self.chapters) < 1:
+            self.isManga = False
+            return
+        if checkIfTankobonByChapter(self.chapters):
+            self.isTanbokon = True
+            self.isSeries = False
+        else:
+            self.isTanbokon = False
+            self.isSeries = True
+
+    def initExtra(self):
+        self.typeName = None
+        self.ended = False
+
+        self.keepTag = True
+        self.dstFolder = ''
+
+    def analysisManga(self, config):
+        self.initExtra()
+        self.analysisMangaType(config)
+        self.queryMangaMapping(config)
+
+    def analysisMangaType(self, config):
+        """ 
+        type: name, folder, tanbokon, series
+        """
+        # 从上往下依次判断
+        for filter in config['filters']:
+            if filter['type'] == 'name':
+                if self.name in filter['names']:
+                    self.typeName = filter['name']
+                if self.name in filter['ended']:
+                    self.ended = True
+            elif filter['type'] == 'folder':
+                for folder in filter['folders']:
+                    if self.fullPath.startswith(folder):
+                        self.typeName = filter['name']
+                        break
+                if self.name in filter['ended']:
+                    self.ended = True
+            elif filter['type'] == 'tanbokon':
+                if self.isTanbokon:
+                    self.typeName = filter['name']
+                if self.name in filter['ended']:
+                    self.ended = True
+            elif filter['type'] == 'series':
+                if self.isSeries:
+                    self.typeName = filter['name']
+                if self.name in filter['ended']:
+                    self.ended = True
+
+    def queryMangaMapping(self, config):
+        """ 查询漫画映射
+        """
+        for map in config['mapping']:
+            if map['name'] == self.typeName:
+                if self.ended:
+                    flag = map['ended']
                 else:
-                    if os.path.exists(cbzfile):
-                        print(f"已经存在cbz文件,直接移动 {cbzfile} 到 {dest}")
-                        renamefile(cbzfile, dest)
+                    flag = map['default']
+                if flag == 'move':
+                    self.dstFolder = map['dst']
+                    self.keepTag = False
+                break
+
+    def moveMangaChapterList(self):
+        """ 获取移动的章节列表
+        """
+        # 修正漫画名字
+        modifiedManga = dict()
+
+        manganame = updateMangaName(self.name)
+        if self.isTanbokon:
+            # 单本 将第一章节直接输出为漫画目录即可
+            sourcepath = os.path.join(self.fullPath, self.chapters[0])
+            newpath = os.path.join(self.dstFolder, manganame, manganame)
+            print(f"单本漫画整理:  {self.chapters[0]} >>> {manganame}")
+            modifiedManga[sourcepath] = newpath
+        else:
+            for ch in self.chapters:
+                newch = updateChapter(ch)
+                oldpath = os.path.join(self.fullPath, ch)
+                newpath = os.path.join(self.dstFolder, manganame, newch)
+                modifiedManga[oldpath] = newpath
+        return modifiedManga
+
+
+def moveManga(config):
+    """
+    -   load all manga folder
+    -   check manga type one-by-one
+    -   analysis move/keep
+    -   keep: pass / move: dst
+    -   fix name and chapter
+    -   zip and move
+    """
+    for src in config['sources']:
+        if not os.path.exists(src):
+            print(f"[!] tachiyomi: src目录异常 {src}")
+            return
+        dirs = os.listdir(src)
+        for entry in dirs:
+            if entry == '@eaDir':
+                print("[x] 忽略群晖文件夹")
+                continue
+            full = os.path.join(src, entry)
+
+            mangaInfo = MangaInfo(full)
+            if mangaInfo.isManga:
+                mangaInfo.analysisManga(config)
+            else:
+                continue
+
+            if mangaInfo.keepTag:
+                continue
+            else:
+                modified = mangaInfo.moveMangaChapterList()
+                if not modified:
+                    print("跳过... \n")
+                    continue
+                print("开始压缩...")
+                for key in modified.keys():
+                    cbzfile = key + '.cbz'
+                    dest = modified.get(key) + '.zip'
+                    if DEBUG_MODE:
+                        print(f"测试模式： {cbzfile} 到 {dest}")
                     else:
-                        zipfolder(key, dest)
-            print(f"整理完成! {full} \n")
+                        if os.path.exists(cbzfile):
+                            print(f"已经存在cbz文件,直接移动 {cbzfile} 到 {dest}")
+                            renamefile(cbzfile, dest)
+                        else:
+                            zipfolder(key, dest)
+                print(f"整理完成! {full} \n")
 
 
-def checkTachiyomiManga(root, dstfolder) -> dict:
+def findMangaChapter(root):
+    """ 获取目录下的所有有效章节
     """
-    处理tachiyomi具体漫画目录
-    :param root:   tachiyomi下载目录里的具体漫画目录
-    :param dstfolder:  整理后的输出目录
-    :return : 整理前后对应路径
-    """
-    print("当前漫画目录:" + root)
-    manganame = os.path.basename(root)
-    # 修正漫画名字
-    manganame = updateMangaName(manganame)
     dirs = os.listdir(root)
-    # 先获取所有章节
     chapters = []
     for entry in dirs:
         if entry == '@eaDir':
@@ -70,33 +174,23 @@ def checkTachiyomiManga(root, dstfolder) -> dict:
                 # print("漫画目录已经是压缩文件")
                 chapter = os.path.splitext(entry)[0]
                 chapters.append(chapter)
-    modifiedManga = dict()
+    return chapters
+
+
+def checkIfTankobonByChapter(chapters):
     # 区分单本,多章节,合集等
-    isSingle = False
+    isTankobon = False
     if len(chapters) == 0:
         print("未检测到章节")
         return None
     elif len(chapters) < 2:
-        if chapters[0] == '_单章节':
+        if chapters[0] == '_单章节' or chapters[0] == '单章节':
             print("是单本漫画")
-            isSingle = True
-        elif chapters[0] == '_Ch. 1':
+            isTankobon = True
+        elif chapters[0] == '_Ch. 1' or chapters[0] == 'Ch. 1':
             print("可能是单本漫画,目前只有一个章节")
-            isSingle = True
-    if isSingle:
-        # 单本 将第一章节直接输出为漫画目录即可
-        sourcepath = os.path.join(root, chapters[0])
-        newpath = os.path.join(dstfolder, manganame, manganame)
-        print(f"单本漫画整理:  {chapters[0]} >>> {manganame}")
-        modifiedManga[sourcepath] = newpath
-    else:
-        print("合集 暂不处理")
-        # for ch in chapters:
-        #     newch = updateChapter(ch)
-        #     oldpath = os.path.join(root, ch)
-        #     newpath = os.path.join(dstfolder, manganame, newch)
-        #     modifiedManga[oldpath] = newpath
-    return modifiedManga
+            isTankobon = True
+    return isTankobon
 
 
 def updateMangaName(orignal):
@@ -173,8 +267,4 @@ if __name__ == "__main__":
 
     DEBUG_MODE = config['dry-run']
 
-    if config['tachiyomi']['enable']:
-        for tachi in config['tachiyomi']['mapping']:
-            src = tachi['src']
-            dst = tachi['dst']
-            moveTachiyomiManga(src, dst)
+    moveManga(config['tachiyomi'])
