cfb1ed06826452d02ff77ea41a3e13231409b586
Mathhew <suwmlee@gmail.com>
2023-09-29 22:51:16 +0800

    Update searchExtraFile.py

--- searchExtraFile.py
+++ searchExtraFile.py
@@ -1,4 +1,4 @@
-#!/usr/local/bin/python3
+#!/bin/python
 # -*- coding: utf-8 -*-
 """ Optimize Movies folder
 
@@ -6,13 +6,27 @@
 检测没有匹配视频的额外多余文件(例如 字幕,nfo,jpg等)
 """
 import os
-
+import shutil
 
 video_type = ['.mp4', '.avi', '.rmvb', '.wmv',
               '.mov', '.mkv', '.flv', '.ts', '.webm', '.iso']
 sub_type = ['.ass', '.srt', '.sub', '.ssa', '.smi', '.idx', '.sup',
             '.psb', '.usf', '.xss', '.ssf', '.rt', '.lrc', '.sbv', '.vtt', '.ttml']
 ext_type = ['.png', '.jpg', '.bmp', '.nfo']
+
+
+def checkFolderhasMedia(folder):
+    """ 检测文件夹内是否有视频文件
+    """
+    if not os.path.isdir(folder):
+        if os.path.exists(folder):
+            return True
+        return False
+    for root, dirs, files in os.walk(folder, topdown=False):
+        for file in files:
+            if file.lower().endswith(tuple(video_type)) and not '.sample.' in file.lower():
+                return True
+    return False
 
 
 def findfolderwithextrasubs(folder, suffix):
@@ -84,8 +98,8 @@ def findfolderwithextraext(folder, suffix):
 
 if __name__ == '__main__':
 
-    folder = input('Please input folder:')
-    # folder = 'E:\\Github\\seedbox\\test'
+    #folder = input('Please input folder:')
+    folder = '/volume1/Media/Movies'
     if folder == '':
         folder = os.getcwd()
         print("will use current folder: " + folder)
@@ -93,18 +107,24 @@ if __name__ == '__main__':
     print("extra subs:")
     subs = findfolderwithextrasubs(folder, sub_type)
 
-    # print("=======================================")
-    # print("extra files:")
-    # imgs = findfolderwithextraext(folder, ext_type)
-
     cleanSub = input('Clean all extra subs Yes/No (default:No):')
     if cleanSub.lower() == "y" or cleanSub.lower() == "yes":
         for sub in subs:
             os.remove(sub)
         print("Clean Subs Done")
-    # cleanExtra = input('Clean all extra files Yes/No (default:No):')
-    # if cleanExtra.lower() == "y" or cleanExtra.lower() == "yes":
-    #     for img in imgs:
-    #         os.remove(img)
-    #     print("Clean Extra Done")
+
+    emptyFolders = []
+    dirs = os.listdir(folder)
+    for entry in dirs:
+        full = os.path.join(folder, entry)
+        if os.path.isdir(full):
+            result = checkFolderhasMedia(full)
+            if not result:
+                print(f"{full} 文件夹内无视频文件")
+                emptyFolders.append(full)
+    cleanFolders = input('Clean all empty folders: Yes/No (default:No):')
+    if cleanFolders.lower() == "y" or cleanFolders.lower() == "yes":
+        for f in emptyFolders:
+            shutil.rmtree(f)
+
     print("Done")
