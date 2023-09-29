a76373eeaaf70ae892ecb515ae4ea80fed12c7f4
Mathhew <suwmlee@gmail.com>
2023-09-30 00:37:06 +0800

    Update zip_manga.py

--- organizefiles/zip_manga.py
+++ organizefiles/zip_manga.py
@@ -1,6 +1,15 @@
-
+#!/bin/python
+# -*- coding: utf-8 -*-
+""" 
+批量压缩漫画 jpg 文件
+"""
 import os
 import zipfile
+
+SRC_FOLDER = "/path/to/src"
+DST_FOLDER = "/path/to/dst"
+ZIP_LVL = 2
+DRY_RUN = True
 
 def zipfolder(srcfolder, destfile):
     """ 压缩 `srcfolder` 目录下的文件到 `destfile`
@@ -28,19 +37,25 @@ def deep_lvl_zip(root, lvl):
     """ 按层级压缩
     """
     dirs = os.listdir(root)
-    for dir in dirs:
-        f = os.path.join(root, dir)
-        if os.path.isdir(f):
+    for dd in dirs:
+        full = os.path.join(root, dd)
+        if os.path.isdir(full):
             if lvl == 1:
-                dstfile = f + ".zip"
-                print(f"{dir}  ===>  {dstfile}")
+                filename = str(dd).replace("卷", "volume_") + ".zip"
+                dstfile = os.path.join(DST_FOLDER, filename)
+                print(f"{dd}  ===>  {dstfile}")
+                if not DRY_RUN:
+                    zipfolder(full, dstfile)
             else:
                 newlvl = lvl - 1
-                deep_lvl_zip(f, newlvl)
+                deep_lvl_zip(full, newlvl)
 
 
 if __name__ == "__main__":
+    DRY_RUN = True
 
-    root = "/xx/xxx/xx"
-    deep_lvl_zip(root, 2)
+    check = input('测试模式 (default:Yes):')
+    if check.lower() == "n" or check.lower() == "no":
+        DRY_RUN = False
+    deep_lvl_zip(SRC_FOLDER, ZIP_LVL)
 
